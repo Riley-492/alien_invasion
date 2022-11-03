78c2e3bfda09aabc9de202c9d3e4f3d0f2e11ee9
Riley-492 <m252382@usna.edu>
2022-11-03 16:12:56 -0400

    Final

--- Aliens game/aliens_invasion.py
+++ Aliens game/aliens_invasion.py
@@ -77,6 +77,8 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
+            #self.sb.prep_ships()
             # Hide the mouse cursor
             pygame.mouse.set_visible(False)
 
@@ -119,6 +121,9 @@ class AlienInvasion:
             self._create_fleet()
             self.settings.increase_speed()
 
+            self.stats.level += 1
+            self.sb.prep_level()
+
         # Get rid of bullets that have disappeared
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
@@ -140,6 +145,7 @@ class AlienInvasion:
         if self.stats.ships_left > 0:
             # Decrement ships_left
             self.stats.ships_left -= 1
+            #self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets
             self.aliens.empty()
--- Aliens game/scoreboard.py
+++ Aliens game/scoreboard.py
@@ -1,8 +1,12 @@
 import pygame.font
+#from pygame.sprite import Sprite
+
+#from ship import Ship
 
 class Scoreboard:
 
     def __init__(self, ai_game):
+        #self.ai_game = ai_game
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
         self.settings = ai_game.settings
@@ -13,6 +17,8 @@ class Scoreboard:
 
         self.prep_score()
         self.prep_high_score()
+        self.prep_level()
+        #self.prep_ships()
 
     def prep_score(self):
         rounded_score = round(self.stats.score, -1)
@@ -23,6 +29,13 @@ class Scoreboard:
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
+    def prep_level(self):
+        level_str = str(self.stats.level)
+        self.level_image = self.font.render(level_str, True, self.text_color, self.settings.bg_color)
+
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
 
     def prep_high_score(self):
         high_score = round(self.stats.high_score, -1)
@@ -33,9 +46,20 @@ class Scoreboard:
         self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = self.score_rect.top
 
+    def prep_ships(self):
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.ai_game)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
+
+
     def show_score(self):
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
+        #self.ships.draw(self.screen)
 
     def check_high_score(self):
         if self.stats.score > self.stats.high_score:
